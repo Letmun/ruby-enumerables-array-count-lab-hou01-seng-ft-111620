cd35257a0b6078f95e859ed9de8f7e03130f92d8
Duncan Stark <letmunplaysgames@gmail.com>
2020-11-11 21:53:44 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,16 +5,3 @@ def count_strings(array)
   array.count
 end
 
-def count_empty_strings(array)
-  array = [ "", "Hello", 4, [], "", "" ]
-  count = 0
-  array.each do |string|
-    if string.is_a? string 
-      if string.empty?
-        count +1
-      end
-    elsif 
-    end
-  end
-  count
-  end 
